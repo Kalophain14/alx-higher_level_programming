418e292118dbe860d3b143d6a230da60eec889bc
‘kalophain14’ <chueutemosho@gmail.com>
2023-11-27 22:25:54 +0200

    changes to tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,29 +2,62 @@
 #include <stdlib.h>
 #include "lists.h"
 
-int find(listint_t *head, int node_ptr)
-{
-	listint_t *temp = head;
-
-	while(temp)
-	{
-		temp = temp->next;
-	}
-}
 /**
- * check_cycle - check if a lincked list contains a cycle.
- * @list: The linked list to check.
- * Return: 1 if the list contains a cycle. 0 otherwise.
- */
-int check_cycle(listint_t *list)
+ *  * print_listint - prints all elements of a listint_t list
+ *   * @h: pointer to head of list
+ *    * Return: number of nodes
+ *     */
+size_t print_listint(const listint_t *h)
 {
-	int cycle_found = 0;
-	listint_t *previous_nodes, *temp;
-	temp = list;
-	while(temp && !cycle_found)
-	{
-		temp = temp->next;
-		cycle_found = find(list, temp);
-	}
-	return cycle_found;
+	    const listint_t *current;
+	        unsigned int n; /* number of nodes */
+
+		    current = h;
+		        n = 0;
+			    while (current != NULL)
+				        {
+						        printf("%i\n", current->n);
+							        current = current->next;
+								        n++;
+									    }
+
+			        return (n);
+}
+
+/**
+ *  * add_nodeint - adds a new node at the beginning of a listint_t list
+ *   * @head: pointer to a pointer of the start of the list
+ *    * @n: integer to be included in node
+ *     * Return: address of the new element or NULL if it fails
+ *      */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	    listint_t *new;
+
+	        new = malloc(sizeof(listint_t));
+		    if (new == NULL)
+			            return (NULL);
+
+		        new->n = n;
+			    new->next = *head;
+			        *head = new;
+
+				    return (new);
+}
+
+/**
+ *  * free_listint - frees a listint_t list
+ *   * @head: pointer to list to be freed
+ *    * Return: void
+ *     */
+void free_listint(listint_t *head)
+{
+	    listint_t *current;
+
+	        while (head != NULL)
+			    {
+				            current = head;
+					            head = head->next;
+						            free(current);
+							        }
 }
