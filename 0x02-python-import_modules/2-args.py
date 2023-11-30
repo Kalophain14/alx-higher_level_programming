0c50ff4f27572213157bc9c46639e6a2bf487825
‘kalophain14’ <chueutemosho@gmail.com>
2023-12-01 00:00:38 +0200

    imports functions from the file calculator_1.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print(f"1 arguments: \n1: {sys.argv[1]}")
+    else:
+        print(f"{len(sys.argv)} arguments:")
+        for i in range(1, len(sys.argv)):
+            print(f"{i}: {sys.argv[i]}")
