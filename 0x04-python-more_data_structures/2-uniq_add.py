df50ba6ce99f59e68269827bf9b0e16df9c81b23
‘kalophain14’ <chueutemosho@gmail.com>
2023-12-06 22:18:19 +0200

    Edited both codes and len version

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    integer = 0
-    if roman_string:
-        conversions_dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000}
-        for c in roman_string:
-            integer += conversions_dict.get(c, 0)
-    return integer
+def uniq_add(my_list=[]):
+    add = 0
+    for i in set(my_list):
+        add += i
+    return add
